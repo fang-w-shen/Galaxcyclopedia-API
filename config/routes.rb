527e33c2c3185d6a9637c295aa2d5eb0ecd15eb2
Fang <fang-wen.shen@vanderbilt.edu>
2017-11-16 15:11:47 -0600

    Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 	get  '/about',   to: 'static_pages#about'
 	get  '/contact', to: 'static_pages#contact'
 	get  '/signup',  to: 'users#new'
-	get 'users/new'
+	post  '/signup',  to: 'users#create'
+
+	resources :users
 
 end
