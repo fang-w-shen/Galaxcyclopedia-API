35f941524d8ebf209da00891d00b8e914550a783
Fang <fang-wen.shen@vanderbilt.edu>
2017-11-19 20:49:17 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+User.create!(name:  "Fang",
+             email: "funkapunkafide@gmail.com",
+             password:              "fang123",
+             password_confirmation: "fang123",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
-end
 
 users = User.order(:created_at).take(6)
 50.times do
