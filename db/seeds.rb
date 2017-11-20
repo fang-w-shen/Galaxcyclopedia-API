dc020a72a06ecff08d6057b1ec72950518a9f203
Fang <fang-wen.shen@vanderbilt.edu>
2017-11-19 20:51:37 -0600

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,18 @@ User.create!(name:  "Fang",
              activated: true,
              activated_at: Time.zone.now)
 
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
+end
+
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
