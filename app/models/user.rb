abfbed29be1db31ac87173afa4f56f5d60d90e1c
Fang <fang-wen.shen@vanderbilt.edu>
2017-11-21 01:39:56 -0600

    added following feeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,14 @@ class User < ApplicationRecord
   uniqueness: { case_sensitive: false }
   has_secure_password
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+  foreign_key: "follower_id",
+  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+  foreign_key: "followed_id",
+  dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -22,7 +30,23 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE  follower_id = #{id}"
+    Micropost.where("user_id IN (#{following_ids})
+     OR user_id = #{id}")
+  end
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
   # Remembers a user in the database for use in persistent sessions.
   def remember
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module RailsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
