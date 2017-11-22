49bca161eef94bdf59d52328a099d331afcb260f
Fang <fang-wen.shen@vanderbilt.edu>
2017-11-22 16:10:40 -0600

    pdf

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,11 +4,11 @@ class Micropost < ApplicationRecord
 	default_scope -> { order(created_at: :desc) }
 	validates :user_id, presence: true
 	validates :content, presence: true, length: { maximum: 140 }
-	validate :picture_size
+	# validate :picture_size
 	private
 	def picture_size
-		# if picture.size > 5.megabytes
-		# 	errors.add(:picture, "should be less than 5MB")
-		# end
+		if picture.size > 5.megabytes
+			errors.add(:picture, "should be less than 5MB")
+		end
 	end
 end
