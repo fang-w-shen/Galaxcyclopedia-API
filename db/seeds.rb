2a53680c3dae2009f94b13bc0d8cd2d9232d8f79
Fang <fang-wen.shen@vanderbilt.edu>
2017-12-07 22:59:18 -0600

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,37 +7,41 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 num = 0
 User.create!(name:  "Fang",
-             email: "funkapunkafide@gmail.com",
-             password:              "fang123",
-             password_confirmation: "fang123",
-             admin:     true,
-             activated: true,
-             activated_at: Time.zone.now,
-             api_key:"#{num=num+1}")
+ email: "funkapunkafide@gmail.com",
+ password:              "fang123",
+ password_confirmation: "fang123",
+ admin:     true,
+ activated: true,
+ activated_at: Time.zone.now,
+ api_key:"#{num=num+1}")
 
 5.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "fang123"
   User.create!(name:  name,
-              email: email,
-              password:              password,
-              password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now,
-              api_key:"#{num=num+1}")
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now,
+    api_key:"#{num=num+1}")
 end
 
 users = User.order(:created_at).take(6)
 
-
-users[0].posts.create!(picture: File.open(File.join(Rails.root, "favicon.ico")))
+users[0].create!(content: content, picture:File.open(File.join(Rails.root, "background.png"))) }
+users[1].create!(content: content, picture:File.open(File.join(Rails.root, "background.png"))) }
+users[2].create!(content: content, picture:File.open(File.join(Rails.root, "background.png"))) }
+users[3].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png"))) }
+users[4].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png"))) }
+users[5].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png"))) }
 
 
 # Following relationships
 users = User.all
 user  = users.first
-following = users[1]
+following = users[1..5]
 followers = users[1..5]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
