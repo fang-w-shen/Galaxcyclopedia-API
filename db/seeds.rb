7099b7bb9e932f600f7cbe77b9a1c80448fcf7a9
Fang <fang-wen.shen@vanderbilt.edu>
2017-12-07 23:02:58 -0600

    reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,12 +30,12 @@ end
 
 users = User.order(:created_at).take(6)
 
-users[0].create!(content: content, picture:File.open(File.join(Rails.root, "background.png"))) }
-users[1].create!(content: content, picture:File.open(File.join(Rails.root, "background.png"))) }
-users[2].create!(content: content, picture:File.open(File.join(Rails.root, "background.png"))) }
-users[3].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png"))) }
-users[4].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png"))) }
-users[5].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png"))) }
+users[0].create!(content: content, picture:File.open(File.join(Rails.root, "background.png")))
+users[1].create!(content: content, picture:File.open(File.join(Rails.root, "background.png")))
+users[2].create!(content: content, picture:File.open(File.join(Rails.root, "background.png")))
+users[3].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png")))
+users[4].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png")))
+users[5].create!(content: content, picture:File.open(File.join(Rails.root, "rails.png")))
 
 
 # Following relationships
